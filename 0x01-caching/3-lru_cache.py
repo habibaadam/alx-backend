0a611911e9cc204180cee39ccc240d77cafc85eb
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-02 20:36:37 +0000

    3. LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""module that contains lru caching system"""
+
+
+from base_caching import BaseCaching
+from datetime import datetime
+
+
+class LRUCache(BaseCaching):
+    """class for fifo caching"""
+
+    def __init__(self):
+        """initializes the class"""
+        super().__init__()
+        self.cache_data_time = {}
+
+    def put(self, key, item):
+        """assigns or puts an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data.update({key: item})
+
+        if len(self.cache_data) == BaseCaching.MAX_ITEMS \
+                and key not in self.cache_data.keys():
+            removed_key = min(self.cache_data_time,
+                              key=self.cache_data_time.get)
+            del self.cache_data_time[removed_key]
+            del self.cache_data[removed_key]
+            print("DISCARD: {}".format(removed_key))
+
+        self.cache_data.update({key: item})
+        self.cache_data_time.update({key: datetime.today()})
+
+    def get(self, key):
+        """Get an item by key"""
+        self.cache_data_time.update({key: datetime.today()})
+        return self.cache_data.get(key)
