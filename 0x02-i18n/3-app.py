5b5dc85d28c20b798bb71de2ee7a79aed539586e
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-04-09 18:02:45 +0000

    testing

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python3
-"""Script containing a basic flask app
-   and a basic babel set up to determine the
-   best match from supported languages and
-   perform translations
+"""A Basic Flask app with internationalization support.
 """
-
+from flask_babel import Babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
-
-app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config:
-    """flask configurations for babel"""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
@@ -32,7 +27,4 @@ def get_locale():
 @app.route('/')
 def hello_world():
     """Method that renders a template"""
-    home_title = _("Welcome to Holberton")
-    home_header = _("Hello world")
-    return render_template("3-index.html", title=home_title,
-                           header=home_header)
+    return render_template('3-index.html')
